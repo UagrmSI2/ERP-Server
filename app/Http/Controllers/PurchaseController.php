2a97f5aacdc8d739f17860aba9bf8abcf8b12bfe
Crissel <cristhianbkick@gmail.com>
2020-08-29 18:22:36 -0400

    Bajando Cambios

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -3,8 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Purchase;
+use Carbon\Carbon;
 
 class PurchaseController extends Controller
 {
-    //
+    public function setNew(Request $request){
+        try{
+        $deposit_id=$request->deposit_id;
+        $purchase=new PurchaseNote();
+        $purchase->fecha=Carbon::now();
+        $purchase->provider_id=$request->provider_id;
+        $purchase->purchase_bill_id=$request->purchase_bill_id;
+        $total_price=0;
+        foreach($request['products'] as $product){
+            $productInfo=Product::where('id',$product['id'])->first();
+            $row=[
+                'purchase_note_id'=>$purchase->id,
+                'product_id'=>$product['id'],
+                'deposit_id'=>$deposit_id,
+                'cantidad'=>$product['cantidad'],
+                'precio'=>$productInfo->precio,
+
+            ];
+            DB::table('purchase_details')->insert($row);
+            $total_price=$total_price+($row['cantidad']*$row['precio']);
+            $deposit_product=DepositProduct::where('product_id',$product['product_id'])->where('deposit_id',$deposit_id)->first();
+            if($deposit_product!=null){
+                $deposit_product->stock=$deposit_product->stock+$product['cantidad'];
+                $deposit_product-save();
+            }else{
+                $deposit_product=new DepositProduct();
+                $deposit_product->product_id=$product['id'];
+                $deposit_product->deposit_id=$deposit_id;
+                $deposit_product->stock=$product['cantidad'];
+                $deposit_product->save();
+            }
+        }
+        $purchase->monto_total=$total_price;
+        $purchase->save();
+        return response()->json('Correcto',200);
+    }catch(Exception $e){
+        return response()->json($e->getMessage(),500);
+    }
+    }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(EmployeeSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(CountrySeeder::class);
-        $this->call(CitySeeder::class);
+        $this->call(CitySeeder::class); 
     }
 }
