9fbd4bf0c592e09f190ff7bbbaa198b89ed173fe
Chon <raydenilsonsantacruz@gmail.com>
2020-08-29 17:32:35 -0400

    segunda parte de los seeders hasta DepositProduct

--- database/seeds/OfficeSeeder.php
+++ database/seeds/OfficeSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Office;
+use Carbon\Carbon;
 class OfficeSeeder extends Seeder
 {
     /**
@@ -11,6 +12,19 @@ class OfficeSeeder extends Seeder
      */
     public function run()
     {
-        
+        $InformacionSucursales = [
+            [
+                "nombre"=>"Sucursal1",
+                "city_id"=>1
+            ],
+        ];
+        foreach($InformacionSucursales as $InformacionSucursal){
+            $sucursal = new Office();
+            $sucursal->nombre = $InformacionSucursal['nombre'];
+            $sucursal->city_id = $InformacionSucursal['city_id'];
+            $sucursal->created_at=Carbon::now();
+            $sucursal->updated_at=Carbon::now();
+            $sucursal->save();
+        }
     }
 }
