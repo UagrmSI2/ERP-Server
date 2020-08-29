a47d17d5c1ac0c472d7ebf18989316453765b8b1
jesuj <jesuz194@gmail.com>
2020-08-29 17:34:54 -0400

    CRUD de Product y Category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,43 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use App\Product;
+use App\Category;
 use Illuminate\Http\Request;
 
-class ProductController extends Controller
+class CategoryController extends Controller
 {
     public function read(){
-        $record=Product::all();
+        $record = Category::all();
         return response()->json($record->toArray(),200);
     }
 
     public function create(Request $request){
-        $record=Product::create([
-            'descripcion' => $request->get('descripcion'),
+        $record = Category::create([
             'nombre' => $request->get('nombre'),
-            'precio' => $request->get('precio'),
-            'costo' => $request->get('costo'),
-            'category_id' => $request->get('category_id'),
+            'descripcion' => $request->get('descripcion')
         ]);
         return response()->json($record->toArray());
     }
 
     public function update(Request $request,$id){
-        $record=Product::find($id);
+        $record = Category::find($id);
         $record->update([
-            'descripcion'=> $request->get('descripcion'),
             'nombre' => $request->get('nombre'),
-            'precio' => $request->get('precio'),
-            'costo' => $request->get('costo'),
-            'category_id' => $request->get('category_id')
+            'descripcion' => $request->get('descripcion'),
         ]);
         return response()->json($record->toArray());
     }
 
     public function destroy($id){
-        $record = Product::find($id);
+        $record = Category::find($id);
         $record->delete();
         return response([],204);
     }
-
 }
