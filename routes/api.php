992ef70953079b3a0471fb80273ac60b0b511243
Crissel <cristhianbkick@gmail.com>
2020-08-30 13:56:25 -0400

    Cambios Bergas de Estos Malditos

--- routes/api.php
+++ routes/api.php
@@ -23,19 +23,19 @@ Route::post('newPurchase','PurchaseController@setNew');
 
 //---------------------Crud Proveedores---------------
 
-Route::post('provider','ProviderController@post');
+Route::post('providers','ProviderController@post');
 Route::put('provider/update/{id}','ProviderController@put');
 Route::get('providers/get','ProviderController@get');
 Route::delete('provider/delete/{id}','ProviderController@delete');
 
 
 //----------------------Crud Categorias---------------
-Route::get('category','CategoryController@read');
+Route::get('categories','CategoryController@read');
 Route::post('category','CategoryController@create');
 Route::put('category/{id}','CategoryController@update');
 Route::delete('category/{id}','CategoryController@destroy');
 //-----------------------Crud Productos-----------------
-Route::get('product','ProductController@read');
+Route::get('products','ProductController@read');
 Route::post('product','ProductController@create');
 Route::put('product/{id}','ProductController@update');
 Route::delete('product/{id}','ProductController@destroy');
